cbf03523108e40ae1ae89dda3bbfbf562c6ab1c1
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-05-27 20:17:44 +1000

    added streaming server completed application

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends Component {
+	constructor(props) {
+		super(props);
+		this.videoRef = React.createRef;
+	}
+
 	componentDidMount() {
+		console.log(this.videoRef);
 		this.props.fetchStream(this.props.match.params.id);
+		this.buildPlayer();
+	}
+
+	componentDidUpdate() {
+		this.buildPlayer();
+	}
+
+	buildPlayer() {
+		if (this.player || !this.props.stream) {
+			return;
+		}
+		this.player = flv.createPlayer({
+			type: "flv",
+			url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+		});
+		this.player.attachMediaElement(this.videoRef.current);
+		this.player.load();
 	}
 
 	render() {
@@ -13,6 +37,7 @@ class StreamShow extends Component {
 		}
 		return (
 			<div>
+				<video ref={this.videoRef} style={{ width: "100%" }} controls />
 				<h1>{this.props.stream.title}</h1>
 				<h5>{this.props.stream.description}</h5>
 			</div>
